77bff020f9e21819fc69e50533f7c86256d8bcdd
okiti <osivwiokiti@gmail.com>
2020-10-29 05:00:09 +0100

    added layout and optimise menu UI

--- src/components/menu/sidebar.vue
+++ src/components/menu/sidebar.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div class="flex flex-col justify-between">
     <nav class="flex flex-col flex-no-wrap">
       <a href="">
-        <i class="uil uil-home"></i>
+        <i class="uil uil-tachometer-fast"></i>
       </a>
       <a href="">
         <i class="uil uil-newspaper"></i>
@@ -11,14 +11,20 @@
         <i class="uil uil-paperclip"></i>
       </a>
     </nav>
+    <nav class="pb-10">
+      <a href="">
+        <i class="uil uil-user-circle"></i>
+      </a>
+    </nav>
   </div>
 </template>
 <style lang="scss" scoped>
 nav {
+  margin-top: 20px;
   a {
     @apply text-ash;
     i {
-      font-size: 34px;
+      font-size: 32px;
     }
   }
 }
